f54b70c95519fbf6321e9eaa2b0112c40c606019
Jonny440 <zh.batyrbekov1204@gmail.com>
2025-01-25 11:46:12 +0500

    update w3schools.py

--- w3schools.py
+++ w3schools.py
@@ -1,41 +1,30 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2820
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue194;\red255\green255\blue255;\red0\green0\blue0;
-\red146\green26\blue32;\red251\green0\blue7;\red15\green112\blue1;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c80392;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c0\c0;
-\cssrgb\c64706\c16471\c16471;\cssrgb\c100000\c0\c0;\cssrgb\c0\c50196\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+#print("Hello, World!")
 
-\f0\fs30 \cf2 \cb3 \expnd0\expndtw0\kerning0
-#print\cf0 (\cf5 "Hello, World!"\cf0 )\
-\
-\cf2 #if\cf0  \cf6 5\cf0  > \cf6 2\cf0 :\cb1 \
-\cb3 #\'a0 \cf2 print\cf0 (\cf5 "Five is greater than two!"\cf0 )\
-\
-#x = \cf6 5\cf0 \cb1 \
-\cb3 #y = \cf5 "John"\cf0 \cb1 \
-\cf2 \cb3 #print\cf0 (\cf2 type\cf0 (x))\cb1 \
-\cf2 \cb3 #print\cf0 (\cf2 type\cf0 (y))\
-\
-#myvar = \cf5 "John"\cf0 \cb1 \
-\cb3 #my_var = \cf5 "John"\cf0 \cb1 \
-\cb3 #_my_var = \cf5 "John"\
-\
-\cf0 #x, y, z = \cf5 "Orange"\cf0 , \cf5 "Banana"\cf0 , \cf5 "Cherry"\
-\
-\cf0 #x = \cf5 "Python "\cf0 \cb1 \
-\cb3 #y = \cf5 "is "\cf0 \cb1 \
-\cb3 #z = \cf5 "awesome"\cf0 \cb1 \
-\cf2 \cb3 #print\cf0 (x + y + z)\
-\
-\cf2 #def\cf0  myfunc():\cb1 \
-\cb3 #\'a0 \cf2 global\cf0  x\cb1 \
-\cb3 #\'a0 x = \cf5 "fantastic"\cf0 \cb1 \
-\
-\cb3 #myfunc()\
-\
-#x = \cf2 int\cf0 (\cf6 1\cf0 )\'a0\'a0 \cf7 # x will be 1\cb1 \
-\cf0 \cb3 #y = \cf2 int\cf0 (\cf6 2.8\cf0 ) \cf7 # y will be 2\cb1 \
-\cf0 \cb3 #z = \cf2 int\cf0 (\cf5 "3"\cf0 )}
+#if 5 > 2:
+#  print("Five is greater than two!")
+
+#x = 5
+#y = "John"
+#print(type(x))
+#print(type(y))
+
+#myvar = "John"
+#my_var = "John"
+#_my_var = "John"
+
+#x, y, z = "Orange", "Banana", "Cherry"
+
+#x = "Python "
+#y = "is "
+#z = "awesome"
+#print(x + y + z)
+
+#def myfunc():
+#  global x
+#  x = "fantastic"
+
+#myfunc()
+
+#x = int(1)   # x will be 1
+#y = int(2.8) # y will be 2
+#z = int("3")
